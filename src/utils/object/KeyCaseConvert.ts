f4537ce83cfdee572b299408d31b26da89d38f34
syu-y <syu@12580526@gmail.com>
2024-04-20 03:17:26 +0900

    スネークケースとキャメルケースの変換ユーティリティ追加

---
+++ src/utils/object/KeyCaseConvert.ts
@@ -0,0 +1,59 @@
+import { strToCamelCase, strToSnakeCase } from '../string/CaseConvert';
+
+/**
+ * キャメルケース変換
+ * スネークケースとケバブケースをキャメルケースに変換
+ * */
+export const toCamelCase = <T extends Record<string, unknown>>(
+  obj: T
+): CamelCase<T> => {
+  if (Array.isArray(obj)) {
+    return obj.map(toCamelCase) as unknown as CamelCase<T>;
+  }
+  if (obj !== null && typeof obj === 'object') {
+    return Object.fromEntries(
+      Object.entries(obj).map(([key, value]) => [
+        strToCamelCase(key),
+        toCamelCase(value as Record<string, unknown>),
+      ])
+    ) as unknown as CamelCase<T>;
+  }
+  return obj as CamelCase<T>;
+};
+
+type CamelCase<T> = T extends Record<string, unknown>
+  ? { [K in keyof T]: CamelCase<T[K]> }
+  : T extends (infer U)[]
+  ? U extends Record<string, unknown>
+    ? CamelCase<U>[]
+    : T
+  : T;
+
+/**
+ * スネークケースへの変換
+ * キャメルケースとケバブケースをスネークケースに変換
+ * */
+export const toSnakeCase = <T extends Record<string, unknown>>(
+  obj: T
+): SnakeCase<T> => {
+  if (Array.isArray(obj)) {
+    return obj.map(toSnakeCase) as unknown as SnakeCase<T>;
+  }
+  if (obj !== null && typeof obj === 'object') {
+    return Object.fromEntries(
+      Object.entries(obj).map(([key, value]) => [
+        strToSnakeCase(key),
+        toSnakeCase(value as Record<string, unknown>),
+      ])
+    ) as unknown as SnakeCase<T>;
+  }
+  return obj as SnakeCase<T>;
+};
+
+type SnakeCase<T> = T extends Record<string, unknown>
+  ? { [K in keyof T]: SnakeCase<T[K]> }
+  : T extends (infer U)[]
+  ? U extends Record<string, unknown>
+    ? SnakeCase<U>[]
+    : T
+  : T;
---
+++ src/utils/string/CaseConvert.ts
@@ -0,0 +1,37 @@
+/**
+ * キャメルケース変換
+ * 変換対象：スネークケース、ケバブケース、パスカルケース
+ * */
+export const strToCamelCase = (value: string) => {
+  // Keyから"-" or "_" + 小文字を探し、大文字に変換し"-"と"_"を削除
+  let result = value.replace(/([-_][a-z])/g, (group) =>
+    group.toUpperCase().replace('-', '').replace('_', '')
+  );
+
+  // パスカルケースの場合は先頭を小文字に変換
+  result = result.charAt(0).toLowerCase();
+
+  return result;
+};
+
+/**
+ * スネークケースへの変換
+ * キャメルケースとケバブケースをスネークケースに変換
+ * */
+export const strToSnakeCase = (value: string) => {
+  // Keyから"-" or 大文字を探し、"_"の場合は"_"に置き換え大文字は小文字に変換して"_"を挿入
+  let result = value.replace(/([-])|([A-Z])/g, (group) => {
+    if (group.includes('-')) {
+      return group.replace('-', '_');
+    } else {
+      return '_' + group.toLowerCase();
+    }
+  });
+
+  // パスカルケースの場合は先頭の"_"を削除
+  if (result.charAt(0).includes('_')) {
+    result = result.replace('_', '');
+  }
+
+  return result;
+};

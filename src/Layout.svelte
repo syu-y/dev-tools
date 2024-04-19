921bb9746acbd3ab2333546432dffdf9c5e06812
syu-y <syu@12580526@gmail.com>
2024-04-19 14:27:55 +0900

    レイアウト・スタイル調整

--- src/Layout.svelte
+++ src/Layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
   import Header from './components/Header.svelte'
+  import Main from './components/Main.svelte'
 </script>
 
-<Header/>
 
-<slot/>
+<div class='font-mono flex flex-col h-screen text-white'>  
+  <div class='sticky top-0 z-50 bg-zinc-800'>
+    <Header/>
+  </div>
+  
+  <div class='bg-gray-950 font-semibold flex-grow p-3'>
+    <Main>
+      <slot/>
+    </Main>
+  </div>
+</div>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,18 @@
 <header>
-	<nav>
-		<a href="/">Home</a>
-		<a href="/case">CASE</a>
+	<nav class='flex border-b justify-between items-center w-full px-2'>
+		<span>
+			<div class='text-2xl font-logo'>
+				<a href='/'>DevTools</a>
+			</div>
+		</span>
+		
+		<div class='flex justify-center items-center gap-4'>
+			<span>
+				<a href="/">Home</a>
+			</span>
+			<span>
+				<a href="/case">CASE</a>
+			</span>
+		</div>
 	</nav>
 </header>
-
-<slot/>

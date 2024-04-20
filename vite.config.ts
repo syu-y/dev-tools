3c5e063e6cb460099d6ecc734399d07e970e21a6
syu-y <syu@12580526@gmail.com>
2024-04-20 19:58:29 +0900

    単体テストの設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  test: {
+    // testの対象ファイル
+    include: ['test/**/*.{test,spec}.{js,ts}'],
+    globals: true,
+    // testの環境
+    environment: 'jsdom',
+  },
 });

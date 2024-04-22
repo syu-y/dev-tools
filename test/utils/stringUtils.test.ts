1e354c8c257b665ef2e39500d40ed81deb039064
syu-y <syu@12580526@gmail.com>
2024-04-23 00:00:20 +0900

    単体テスト追加（文字列操作ユーティリティ）

--- test/utils/stringUtils.test.ts
+++ test/utils/stringUtils.test.ts
@@ -15,7 +15,7 @@ describe('StringUtils.ts', async () => {
     expect(replacedStr).toEqual(correctStr);
   });
 
-  // 文字判定テスト
+  // 文字判定
   test('大文字判定', async () => {
     const result = StringUtils.isUpperCase('A');
     expect(result).toBeTruthy();
@@ -37,5 +37,30 @@ describe('StringUtils.ts', async () => {
   test('文字列の長さをカウント', async () => {
     const result = StringUtils.getCharacterLength('ABCDE');
     expect(result).toBe(5);
+  });
+
+  test('文字列の長さをカウント（サロゲート文字・異体字考慮）', async () => {
+    const surrogatePair = StringUtils.getDisplayCharacterLength('𠮷野家');
+    expect(surrogatePair).toBe(3);
+    const variousChr = StringUtils.getDisplayCharacterLength('葛城市');
+    expect(variousChr).toBe(3);
+    const emoji = StringUtils.getDisplayCharacterLength('🌝👍✈');
+    expect(emoji).toBe(3);
+  });
+
+  test('特定文字列の出現回数をカウント', async () => {
+    const result = StringUtils.getSpecificharacterCount('ABXXCDXXEFGXXH', 'XX');
+    expect(result).toBe(3);
+  });
+
+  test('バイト数をカウント', async () => {
+    const result = StringUtils.getBytes('ABCDEFG');
+    expect(result).toBe(7);
+    const surrogatePair = StringUtils.getBytes('𠮷野家');
+    expect(surrogatePair).toBe(10);
+    const variousChr = StringUtils.getBytes('葛城市');
+    expect(variousChr).toBe(9);
+    const emoji = StringUtils.getBytes('🌝👍✈');
+    expect(emoji).toBe(11);
   });
 });
